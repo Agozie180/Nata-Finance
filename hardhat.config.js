3a8cda78428db0e10100ab6ea5f26f9a715b4ca7
Agozie180 <okaforbartholomew180@gmail.com>
2026-05-29 17:46:15 +0100

    Add deployed contract address

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,24 +1,13 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
 
-const PRIVATE_KEY = process.env.PRIVATE_KEY || "";
-const ARC_RPC_URL = process.env.ARC_RPC_URL || "https://rpc.testnet.arc.network";
-
 module.exports = {
-  solidity: {
-    version: "0.8.20",
-    settings: {
-      optimizer: {
-        enabled: true,
-        runs: 200
-      }
-    }
-  },
+  solidity: "0.8.20",
   networks: {
-    arcTestnet: {
-      url: ARC_RPC_URL,
+    arc: {
+      url: "https://rpc.testnet.arc.network",
       chainId: 5042002,
-      accounts: PRIVATE_KEY ? [PRIVATE_KEY] : []
+  accounts: ["0x" + process.env.PRIVATE_KEY]
     }
   }
 };
